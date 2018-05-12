2fd6e8229adb3eba7bc09a9f3a65f91612123532
Benjamin <benshanahan1@gmail.com>
2018-05-12 17:57:38 +0000

    transmitter testing

--- _old/_testing/telemetry_bandwidth_test/transmitter.py
+++ _old/_testing/telemetry_bandwidth_test/transmitter.py
@@ -4,14 +4,25 @@ from the rocket, in-flight.
 """
 
 from sys import path
-path.insert("../../../lib/telemetry.py")
+path.insert(0, "../../../lib")
 
-MESSAGE_SIZE = 1  # size of message in bytes
+from telemetry import Telemetry
+from time import sleep, time
+
+MESSAGE_SIZE = 1     # size of message in bytes
+MESSAGE_RATE = 5    # message interval in Hertz
+N_MESSAGES   = 20  # number of messages to send
 
 if __name__ == "__main__":
 
     # Create and connect to telemetry radio.
     transmitter = Telemetry()
 
-    while True:
+    print "Sending {} messages at {} Hz.".format(N_MESSAGES, MESSAGE_RATE)
+    print "Estimated total time: {} seconds.".format(float(N_MESSAGES) / float(MESSAGE_RATE))
+    startTime = time()
+    for m in range(N_MESSAGES):
         transmitter.write("x" * MESSAGE_SIZE)
+        sleep(1.0 / float(MESSAGE_RATE))
+
+    print "Sent all messages in {} seconds.".format(time() - startTime)
